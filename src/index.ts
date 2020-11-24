b94bb7ddee93742d2ee5fb7fe99133c056e0ec2e
zhmhbest <zhmhbest@gmail.com>
2020-11-24 20:07:13 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import {Builder, By, Key, until} from "selenium-webdriver";
 (async function() {
     const driver = new Builder().forBrowser('chrome').build();
     await driver.get('https://www.baidu.com');
-    driver.findElement(By.id('kw')).sendKeys('Selenium');
-    driver.findElement(By.id('su')).click();
-    await driver.wait(until.titleIs('Selenium_百度搜索'), 1000);
+    await driver.findElement(By.id('kw')).sendKeys('Selenium');
+    await driver.findElement(By.id('su')).click();
+    await driver.wait(until.titleIs('Selenium_百度搜索'), 2000);
     driver.quit();
 })();
